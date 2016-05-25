14d3dae93a8b57a939fea66a7d00f6b6ff142990
gaoyang <gaoyang_job@sohu.com>
2016-05-25 16:39:43 +0800

    Merge branch 'master' of https://github.com/gaoyang821112/nodejs
    # Conflicts:
    #	util/constants.js

--- service/comment_service.js
+++ service/comment_service.js
@@ -4,7 +4,6 @@
 var redis_util = require('../util/redis_util');
 var logger = require('../util/logger');
 var mongodb = require('../util/mongodb');
-var ObjectId = mongodb.ObjectId;
 var ResponseVo = require('../model/ResponseVo');
 
 var util = require('util');
@@ -40,14 +39,14 @@ function findCommentByArticleIdPagesForPage(req, res) {
         mongodb.comment_collection.count({article_id: articleId}, function (err, count) {
             var comment = new CommentVo(docs, pageNum, pageSize, count);
             var resVo = new ResponseVo(200, comment);
-            resVo.articleId=articleId;
+            resVo.articleId = articleId;
 
             var command = new Array();
             for (var i = 0; i < docs.length; i++) {
 
-                var now=new Date().getTime();
-                var nt=convertTime(now,docs[i].create_time);
-                docs[i].create_time=nt;
+                var now = new Date().getTime();
+                var nt = convertTime(now, docs[i].create_time);
+                docs[i].create_time = nt;
 
                 command.push(["hget", "MemberBaseInfo:" + docs[i].uid_comment, 'nickname']);
             }
@@ -59,9 +58,9 @@ function findCommentByArticleIdPagesForPage(req, res) {
                             var nn = rep[i].substr(0, 3) + "****" + rep[i].substr(7, 4);
                             docs[i].nick = nn;
                         } else {
-                            if(err){
+                            if (err) {
                                 docs[i].nick = "游客";
-                            }else{
+                            } else {
                                 docs[i].nick = rep[i];
                             }
                         }
@@ -75,39 +74,39 @@ function findCommentByArticleIdPagesForPage(req, res) {
         });
     });
 };
-var minuteOfSeconds=60;
-var hourOfSeconds=3600;
-var dayOfSeconds=hourOfSeconds*24;
-var weekOfSeconds=dayOfSeconds*7;
+var minuteOfSeconds = 60;
+var hourOfSeconds = 3600;
+var dayOfSeconds = hourOfSeconds * 24;
+var weekOfSeconds = dayOfSeconds * 7;
 
-function convertTime(now,l){
-    var span=now-l;
-    var spanSecond=span/1000;
-    if(spanSecond<minuteOfSeconds){
+function convertTime(now, l) {
+    var span = now - l;
+    var spanSecond = span / 1000;
+    if (spanSecond < minuteOfSeconds) {
         return "刚刚";
-    }else if (spanSecond<hourOfSeconds){
-        var minute=spanSecond/minuteOfSeconds;
-        return Math.floor(minute)+"分钟前";
-    }else if (spanSecond<dayOfSeconds){
-        var hour=spanSecond/hourOfSeconds;
-        return Math.floor(hour)+"小时前";
-    }else if(spanSecond<weekOfSeconds){
-        var day=spanSecond/dayOfSeconds;
-        return Math.floor(day)+"天前";
-    }else{
-        var da=new Date(l);
-        var re=da.getFullYear()+":";
-        var lmon=da.getMonth()+1;
-        if(lmon<10){
-            re=re+"0"+lmon+":";
-        }else{
-            re=re+lmon+":";
+    } else if (spanSecond < hourOfSeconds) {
+        var minute = spanSecond / minuteOfSeconds;
+        return Math.floor(minute) + "分钟前";
+    } else if (spanSecond < dayOfSeconds) {
+        var hour = spanSecond / hourOfSeconds;
+        return Math.floor(hour) + "小时前";
+    } else if (spanSecond < weekOfSeconds) {
+        var day = spanSecond / dayOfSeconds;
+        return Math.floor(day) + "天前";
+    } else {
+        var da = new Date(l);
+        var re = da.getFullYear() + ":";
+        var lmon = da.getMonth() + 1;
+        if (lmon < 10) {
+            re = re + "0" + lmon + ":";
+        } else {
+            re = re + lmon + ":";
         }
-        var lday=da.getDate();
-        if(lday<10){
-            re=re+"0"+lday+":";
-        }else{
-            re=re+lday;
+        var lday = da.getDate();
+        if (lday < 10) {
+            re = re + "0" + lday + ":";
+        } else {
+            re = re + lday;
         }
 
         return re;
@@ -181,10 +180,10 @@ function findCommentByArticleIdPagesFromTime(req, res) {
 
 };
 
-function excludeXss(content){
-    content=content.replace(new RegExp("<","gm"),"");
-    content=content.replace(new RegExp(">","gm"),"");
-    content=content.replace(new RegExp("/","gm"),"");
+function excludeXss(content) {
+    content = content.replace(new RegExp("<", "gm"), "");
+    content = content.replace(new RegExp(">", "gm"), "");
+    content = content.replace(new RegExp("/", "gm"), "");
     return content;
 }
 exports.findCommentByArticleIdPagesFromTime = findCommentByArticleIdPagesFromTime;
@@ -198,7 +197,7 @@ exports.saveComment = function (req, res) {
     var userId = Number(req.body.userId);
     var content = req.body.content;
 
-    content=excludeXss(content);
+    content = excludeXss(content);
 
     if (!isNaN(userId) && !isNaN(articleId) && userId > 0 && articleId > 0 && content && content.length > constants.commentword_min) {
         mongodb.banUser_collection.find({uid_disable: userId}, function (err, docs) {
@@ -210,7 +209,7 @@ exports.saveComment = function (req, res) {
             }
 
             //determine if comment is too frequent
-            var userFrequent = constants.commentfrequent_prefix + articleId+"_userId_"+userId;
+            var userFrequent = constants.commentfrequent_prefix + articleId + "_userId_" + userId;
             redis_util.getClient.get(userFrequent, function (err, rep) {
                 if (rep) {//comment is too frequent
                     res.send(new ResponseVo(10100));
@@ -223,19 +222,19 @@ exports.saveComment = function (req, res) {
                 var comment = new Comment(articleId, userId, content, raw, '1');
                 mongodb.comment_collection.save(comment);
                 redis_util.getClient.set(userFrequent, 1);
-                redis_util.getClient.expire(userFrequent,constants.commentfrequent_expire);
+                redis_util.getClient.expire(userFrequent, constants.commentfrequent_expire);
                 logger.info('save comment 为' + comment.articleId + ' 成功');
                 comment.rawContent = '';
 
-                redis_util.getClient.hget("MemberBaseInfo:"+userId, 'nickname',function (err, rep) {
+                redis_util.getClient.hget("MemberBaseInfo:" + userId, 'nickname', function (err, rep) {
                     if (rep) {
                         if (telReg.test(rep)) {
                             var nn = rep.substr(0, 3) + "****" + rep.substr(7, 4);
                             comment.nick = nn;
                         } else {
-                            if(err){
+                            if (err) {
                                 comment.nick = "游客";
-                            }else {
+                            } else {
                                 comment.nick = rep;
                             }
 
--- util/constants.js
+++ util/constants.js
@@ -1,11 +1,22 @@
-exports.db_connection = 'mongodb://192.168.7.249:27017/comment';
+//----------------------测试环境mongodb-----------------------------
+ exports.db_connection = 'mongodb://192.168.7.249:27017/comment';
+
+//----------------------线上环境mongodb-----------------------------
+//exports.db_connection = 'mongodb://192.168.9.55:27017/comment';
 exports.comment_collection = 'comment_article';
 exports.banUser_collection = 'comment_disable_user';
 exports.sensitive_collection = 'comment_sensitive_word';
 exports.sensitive_path='';
+
+//----------------------测试环境reids-----------------------------
 exports.redis_connection_ip = '192.168.7.249';
 exports.redis_connection_port = '6379';
 exports.redis_connection_pwd = '888888';
+
+//----------------------线上环境reids-----------------------------
+// exports.redis_connection_ip = '192.168.9.20';
+// exports.redis_connection_port = '6378';
+// exports.redis_connection_pwd = '888888';
 
 exports.commentfrequent_prefix='comment_frequent_';
 exports.commentfrequent_expire=10;
