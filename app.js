5e74c8b4bf566d12c318d4f780586c1493a598bb
yaoming6046 <yaoming6046@gmail.com>
2016-05-30 16:08:25 +0800

    commit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/nodejs/', routes);
 app.use('/nodejs/comment', comment);
 app.use('/nodejs/page', page);
-app.use('/prize', prize);
+app.use('/nodejs/prize', prize);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
