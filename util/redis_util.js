d62398e3e64cec6d73557ee5cb0235b4a2fbf223
gaoyang <gaoyang_job@sohu.com>
2016-06-02 14:34:20 +0800

    加redis连接池

--- util/redis_util.js
+++ util/redis_util.js
@@ -1,4 +1,28 @@
 var Constants = require('./constants');
 var redis = require('redis');
-
-exports.getClient = redis.createClient(Constants.redis_connection_port, Constants.redis_connection_ip, {password : Constants.redis_connection_pwd});
+// exports.getClient = redis.createClient(Constants.redis_connection_port, Constants.redis_connection_ip, {password : Constants.redis_connection_pwd});
+var poolModule = require('generic-pool');
+var redispool = poolModule.Pool({
+    name     : 'redis',
+    create   : function(callback) {
+        var client = redis.createClient(Constants.redis_connection_port, Constants.redis_connection_ip, {password : Constants.redis_connection_pwd});
+        callback(null, client);
+    },
+    destroy  : function(client) { client.quit(); },
+    max      : 10,
+    // optional. if you set this, make sure to drain() (see step 3)
+    min      : 2,
+    // specifies how long a resource can stay idle in pool before being removed
+    idleTimeoutMillis : 30000
+    // if true, logs via console.log - can also be a function
+    // log : true
+});
+redispool.acquire(function(err, client) {
+    if (err) {
+        // handle error - this is generally the err from your
+        // factory.create function
+    }
+    else {
+       exports.getClient = client;
+    }
+});
