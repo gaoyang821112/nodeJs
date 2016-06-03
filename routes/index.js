4b95a77154d03bda0eb85ffe912a3fc32a096698
yaoming6046 <yaoming6046@gmail.com>
2016-06-03 09:41:47 +0800

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var indexService = require('../service/indexService.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/ymtest/run', function(req, res, next) {
+  indexService.runComment();
 });
 
 module.exports = router;
